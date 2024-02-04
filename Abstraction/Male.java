5741a778358ed1f8bafb3467063201258c8f075e
Jamiul Abedin Shovon <51233770+jamiulshovon@users.noreply.github.com>
2024-02-04 16:53:43 +0600

    Abstraction

---
+++ Abstraction/Male.java
@@ -0,0 +1,9 @@
+public abstract class Male {
+    int x = 10;
+
+    public abstract void print();
+
+    public void display() {
+        System.out.println("I am Male");
+    }
+}
---
+++ Abstraction/Shovon.java
@@ -0,0 +1,13 @@
+public class Shovon extends Male {
+
+    public void print() {
+        System.out.println(" I am Shovon");
+    }
+
+    public static void main(String[] args) {
+        Shovon person = new Shovon();
+        person.print();
+        person.display();
+    }
+
+}
